82be149df1402c374a1ee8cd4a879723188dc0ec
sam <katana.kenzo@gmail.com>
2019-09-07 21:14:48 +0200

    first commit

---
+++ Client.php
@@ -0,0 +1,100 @@
+
+<!DOCTYPE html>
+
+<head>
+
+    <meta charset="UTF-8">
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Bank Perso</title>
+    <style>
+        body{
+
+            background:url("perso.jpg");
+            background-position:center;
+            background-repeat: no-repeat;
+        }
+        .lala{
+            width: 800px;
+            height: auto;
+            background-color: #ffffff;
+            border: solid white 5px;
+        }
+        .lalo{
+               width: 300px;
+               height: auto;
+               background-color: #ffffff;
+               border: solid #91f0ff 1px;
+            margin: 5px;
+            padding: 5px;
+           }
+    </style>
+</head>
+</html>
+    <body>
+
+        <div class="container mt-4 border-2 lala ">
+            <h1>Bankers group fondation</h1>
+
+            <div class="col-md-10 row">
+
+                <div class="col-md-6" >    <a href="index.php">
+                    <span> <img src="Contenu/b.jpg" width='300px' heigth='300px' alt=""></span>
+                    </a>
+                </div>
+
+                <div class="col-md-6" ><span> <img src="Contenu/logo.jpg" width='280px' heigth='280px' alt=""></span></div>
+
+            </div>
+
+            <div class="col-md-12 row mb-4">
+                <div class="col-md-6">
+                    <span class="btn btn-success">
+                        <p> <strong>Nom </strong>: dzdzdz   <strong>Numero de compte</strong> :dzdzdz</p>
+                    </span>
+
+                    </f>
+                    <form action="">
+                        <label for="Solde"> Motant</label><br>
+                        <input type="text" name="motant">
+
+                        <button class="btn btn-danger"> Retrait</button>
+                        <button class="btn btn-success"> Ajouter</button>
+
+                    </form>
+
+                    <p> <strong>Sodle</strong> :dzdzdz</p>
+                </div>
+                <div class="col-md-6">
+                    <div class="lalo">
+                    <div><strong> Solde </strong> :45555   <strong class="text-danger">$ </strong><strong> Date </strong>: 11 / 45/ 44</div>
+                    <div><strong> Solde </strong> :45555   <strong class="text-danger">$ </strong><strong> Date </strong>: 11 / 45/ 44</div>
+                    <div><strong> Solde </strong> :45555   <strong class="text-danger">$ </strong><strong> Date </strong>: 11 / 45/ 44</div>
+                    <div><strong> Solde </strong> :45555   <strong class="text-danger">$ </strong><strong> Date </strong>: 11 / 45/ 44</div>
+                    <div><strong> Solde </strong> :45555   <strong class="text-danger">$ </strong><strong> Date </strong>: 11 / 45/ 44</div>
+                    <div><strong> Solde </strong> :45555   <strong class="text-danger">$ </strong><strong> Date </strong>: 11 / 45/ 44</div>
+                    <div><strong> Solde </strong> :45555   <strong class="text-danger">$ </strong><strong> Date </strong>: 11 / 45/ 44</div>
+                    <div><strong> Solde </strong> :45555   <strong class="text-danger">$ </strong><strong> Date </strong>: 11 / 45/ 44</div>
+
+                    </div>
+
+
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+
+    </body>
+<html>
---
+++ index.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE html>
+
+<head>
+
+    <meta charset="UTF-8">
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Bank Perso</title>
+
+    <style>
+        body{
+
+            background:url("perso.jpg");
+            background-position:center;
+        }
+    .lala{
+        width: 800px;
+        height: 900px;
+        background-color: #ffffff;
+        border: solid white 5px;
+    }
+    </style>
+</head>
+</html>
+    <body>
+
+        <div class="container mt-4 border-2 lala ">
+            <h1>Bankers group fondation</h1>
+
+            <div class="col-md-12 row">
+                <div class="col-md-6" > <span> <img src="Contenu/b.jpg" width='300px' heigth='300px' alt=""></span></div>
+
+                <div class="col-md-6" ><span> <img src="Contenu/logo.jpg" width='280px' heigth='280px' alt=""></span></div>
+            </div>
+
+            <div class="col-md-12 row">
+            <div class="col-md-8 ">
+                <h3>Liste clients </h3>
+                <!--la boucle commence ici -->
+                <div class="col-md-12 row mb-4">
+                <div class="col-md-8">
+                    <a href="Client.php">
+                    <span class="btn btn-success">
+                        <p> <strong>Nom </strong>: dzdzdz</p>
+                        <p> <strong>Numero de compte</strong> :dzdzdz</p>
+                    </span>
+                    </a>
+                </div>
+                <div class="col-md-2">
+                    <span class="btn btn-danger">
+
+                        <p> <strong>sodle</strong> :dzdzdz</p>
+                    </span>
+
+
+                </div>
+                </div>
+
+
+                <!--la boucle sarret ici -->
+            </div>
+
+
+            <div class="col-md-4">
+                <img src="perso.jpg" width="350" height="250" alt="humain">
+            </div>
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+
+    </body>
+<html>
